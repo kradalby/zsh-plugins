c09ead1bdda970c1795780b677978c30e633125c
Kristoffer Dalby <kradalby@kradalby.no>
2016-04-26 15:29:19 +0200

    other rationalise-dot version;

--- rationalise-dot.zsh
+++ rationalise-dot.zsh
@@ -1,21 +1,11 @@
-# Auto-expand "..." to "../..", "...." to "../../.." etc.
-# It skips certain commands (git, tig, p4).
-#
-# resolve-alias is defined in a separate function.
-
-local MATCH # keep the regex match from leaking to the environment.
-
-# Skip pasted text.
-if (( PENDING > 0 )); then
-  zle self-insert
-  return
-fi
-
-if [[ $LBUFFER =~ '(^|/| |	|'$'\n''|\||;|&)\.\.$' ]] \
-  && ! [[ $(resolve-alias $LBUFFER) =~ '^(git|tig|p4)' ]]; then
-  LBUFFER+=/
-  zle self-insert
-  zle self-insert
-else
-  zle self-insert
-fi
+# This was found on http://grml.org/zsh/zsh-lovers.html
+# Basically type '...' to get '../..' with successive .'s adding /..
+rationalise-dot() {
+  if [[ $LBUFFER = *.. ]]; then
+    LBUFFER+=/..
+  else
+    LBUFFER+=.
+  fi
+}
+zle -N rationalise-dot
+bindkey . rationalise-dot
